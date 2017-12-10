359f90f3ea1fd106880f01d8d925d35dbd6173be
Maciej Buczyński <kadlubken5@gmail.com>
2017-12-10 13:53:58 +0100

    Added template for specified function individual

---
+++ Function.py
@@ -0,0 +1,29 @@
+from base import Individual
+AbstractMethodError = NotImplementedError("You must override this method")
+
+class Function(Individual):
+    """Represents an individual in a population"""
+
+    def __init__(self):
+        """Construct a random individual."""
+        raise AbstractMethodError
+
+    @staticmethod
+    def get_fitness(individual):
+        """Return the fitness value for the given individual."""
+        raise AbstractMethodError
+
+    @staticmethod
+    def mutate(individual, probability):
+        """Mutate the given individual with the given probability."""
+        raise AbstractMethodError
+
+    @staticmethod
+    def crossover(parent1, parent2):
+        """Perform crossover with given parents."""
+        raise AbstractMethodError
+
+    @staticmethod
+    def makecopy(individual):
+        """Return a copy of the individual."""
+        raise AbstractMethodError
